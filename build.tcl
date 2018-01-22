618e7cb01ab9332c9b06299d9964be9c80565d9e
Danilo Chang <ray2501@gmail.com>
2018-01-22 16:35:08 +0800

    Update build.tcl

--- build.tcl
+++ build.tcl
@@ -2,10 +2,6 @@
 
 set arch "noarch"
 set base "tclsoap-tclsoap"
-set fileurl "http://tclsoap.cvs.sourceforge.net/viewvc/tclsoap/tclsoap/?view=tar"
-
-set var [list wget $fileurl -O $base.tar.gz]
-exec >@stdout 2>@stderr {*}$var
 
 if {[file exists build]} {
     file delete -force build
@@ -16,7 +12,3 @@ file copy -force $base.tar.gz build/SOURCES
 
 set buildit [list rpmbuild --target $arch --define "_topdir [pwd]/build" -bb tclsoap.spec]
 exec >@stdout 2>@stderr {*}$buildit
-
-# Remove our source code
-file delete $base.tar.gz
-
